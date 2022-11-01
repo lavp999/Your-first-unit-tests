6011fb308bebba27711d5fb9026e5a6505c7560b
lavp999 <lavp99@hotmail.com>
2022-11-01 13:58:53 +0000

    terminado y remote

---
+++ app.js
@@ -0,0 +1,50 @@
+// esta es mi función que suma dos números
+const sum = (a,b) => {
+    return a + b
+}
+
+// solo un registro en consola para nosotros.
+console.log(sum(7,3))
+
+
+
+// one euro is:
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
+}
+
+function fromEuroToDollar(euro) {
+    let dollar = euro * oneEuroIs.USD;
+    return dollar;
+}
+
+function fromDollarToYen(dollar) {
+    let yen = dollar * oneEuroIs.JPY / oneEuroIs.USD;
+    return yen;
+}
+
+function fromYenToPound(yen) {
+    let pound = yen * oneEuroIs.GBP / oneEuroIs.JPY;
+    return pound;
+}
+
+
+function fromTo(valor, moneda1, moneda2) {
+    let nuevoValor = 0;
+    if(moneda1 == "EUR"){  // habría que hacer el de moneda2
+        nuevoValor = valor * oneEuroIs[moneda2];
+    }else {
+        nuevoValor = valor * oneEuroIs[moneda2] / oneEuroIs[moneda1];
+    }
+
+    return nuevoValor;
+}
+
+
+console.log("Mi resultado :", fromTo(5));
+
+// exporta la función para usarla en otros archivos 
+// (similar a la palabra clave `export` cuando se usa webpack)
+module.exports = { sum , fromEuroToDollar , fromDollarToYen , fromYenToPound , fromTo};
---
+++ test.js
@@ -0,0 +1,47 @@
+// importar la función sum del archivo app.js
+const { sum , fromEuroToDollar , fromDollarToYen , fromYenToPound , fromTo} = require('./app.js');
+
+// comienza tu primera prueba
+test('adds 14 + 9 to equal 23', () => {
+    //dentro de la prueba llamamos a nuestra función sum con 2 números
+    let total = sum(14, 9);
+
+    // esperamos que la suma de esos 2 números sea 23
+    expect(total).toBe(23);
+});
+
+// -----   fromEuroToDollar  -------
+test('1 Euro son 1,2 Dolares', () => {
+    let alCambio = fromEuroToDollar(5);
+    expect(alCambio).toBe(6);  // 5€ deben ser 6$
+});
+
+// -----   fromDollarToYen  -------
+test('1 $ es 106,58333 Yenes', () => {
+    let alCambio = fromDollarToYen(5);
+    expect(alCambio).toBe(532.9166666666667);  // 5$ deben ser 532,916667 yenes
+});
+
+// -----   fromYenToPound  -------
+test('1 Libra son 0,006254887 Yenes', () => {
+    let alCambio = fromYenToPound(5);
+    expect(alCambio).toBe(0.03127443315089914);  // 5 Yenes deben ser 0.03127443 Libras
+});
+
+
+// -----   fromTo  -------
+test('1 Euro son 1,2 Dolares', () => {
+    let alCambio = fromTo(5, "EUR", "USD");
+    expect(alCambio).toBe(6);  // 5€ deben ser 6$
+});
+// -----   fromTo  -------
+test('1 $ es 106,58333 Yenes', () => {
+    let alCambio = fromTo(5, "USD", "JPY");
+    expect(alCambio).toBe(532.9166666666667);  // 5$ deben ser 532,916667 yenes
+});
+// -----   fromTo  -------
+test('1 Libra son 0,006254887 Yenes', () => {
+    let alCambio = fromTo(5 , "JPY" , "GBP"); 
+    expect(alCambio).toBe(0.03127443315089914);  // 5 Yenes deben ser 0.03127443 Libras
+});
+
